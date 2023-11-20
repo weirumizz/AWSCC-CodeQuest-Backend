167f54206e26248b8ba46a90d3584c8db819698c
Mae Go <148067518+weirumizz@users.noreply.github.com>
2023-11-20 18:28:30 +0800

    added value to variables

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,6 @@
-name = None
-age = None
-isMale = None
-department = None
+name = "Wilmae"
+age = 20
+isFem = "female"
+department = "Backend"
 
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"{name} is a {age}-year old {isFem} participating in {department}'s 30 Days of Coding.")
