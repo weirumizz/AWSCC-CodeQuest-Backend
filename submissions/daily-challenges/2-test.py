a669941153404b28c2e4cbeb7aae52629adc2d43
Mae Go <148067518+weirumizz@users.noreply.github.com>
2023-11-21 15:17:18 +0800

    learned data types and input funtions

---
+++ submissions/daily-challenges/2-test.py
@@ -0,0 +1,7 @@
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+third = input("Enter the third number: ")
+
+sum = int(first) + int(second) + int(third) 
+
+print(f"Sum: {sum}")
