1e5e91cd082cf890b07b9cdee352b043257f4f12
Mae Go <148067518+weirumizz@users.noreply.github.com>
2023-11-21 14:53:37 +0800

    made a copy to the new folder

---
+++ submissions/daily-challenges/1-test.py
@@ -0,0 +1,6 @@
+name = "Wilmae"
+age = 20
+isMale = False
+department = "Backend Department"
+
+print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
